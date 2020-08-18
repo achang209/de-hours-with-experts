4b7c728795f2852e752d276ce5bf481e7e1e2794
Alex Chang <achang209@gmail.com>
2020-08-18 15:25:07 -0500

    create methods to convert number to list and vice versa

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 import sys
 
+def num_to_list(number):
+    """function to help convert a multi-digit interger to a list of single digit intergers"""
+    result = []
+    for i in str(number):
+        result.append(int(i))
+    return result
+
+def list_to_num(num_list):
+    """function to help convert a list of single digit integers to a single multi-digit integer"""
+    num_list = [str(i) for i in num_list]
+    num_str = "".join(num_str)
+    result = int(num_str)
+
 def main():
     next_biggest_number(sys.argv[1])
 
